78a4340b78b093a2e33ea96617cf4d762ba38bb5
brucenielson <brucenielson1@gmail.com>
2015-03-21 18:53:33 -0600

    Changed code to use Xlxswriter instead, so that I could add charts.

--- stockexcel.py
+++ stockexcel.py
@@ -1,6 +1,8 @@
 import stockquotes
-import xlwt
+#import xlwt
+import xlsxwriter
 import os
+
 
 def create_stock_worksheet(stock_symbol):
 
@@ -13,24 +15,19 @@ def create_stock_worksheet(stock_symbol):
     current_price = quote['LastTradePriceOnly']
     year_low = quote['YearLow']
 
-    # Create formats
-    date_format = xlwt.XFStyle()
-    date_format.num_format_str = 'dd-mmm-yyyy'
-    dollar_format = xlwt.XFStyle()
-    dollar_format.num_format_str = "[$$-409]#,##0.00;-[$$-409]#,##0.00"
-    percent_format = xlwt.XFStyle()
-    percent_format.num_format_str = "0.00%" #"0.00\\%"
-    title_format = xlwt.XFStyle()
-    font = xlwt.Font()
-    font.name = 'Times New Roman'
-    font.height = 18 * 20 # size * twips (=20 per point)
-    title_format.font = font
-
     # Create workbook
-    stock_book = xlwt.Workbook(encoding="utf-8")
+    stock_book = xlsxwriter.Workbook(os.path.dirname(__file__)+"\\spreadsheets\\"+stock_symbol+".xlsx")
+
+
+    # Create formats
+    title_format = stock_book.add_format({'bold': True})
+    title_format.set_size(18) # size * twips (=20 per point)
+    dollar_format = stock_book.add_format({'num_format': '$#,##0.00'})
+    date_format = stock_book.add_format({'num_format': 'dd-mmm-yyyy'})
+    percent_format = stock_book.add_format({'num_format': '0.00%'})
 
     # Create worksheet with basic stock quote information
-    quote_sheet = stock_book.add_sheet("Stock Quote")
+    quote_sheet = stock_book.add_worksheet("Stock Quote")
 
     # Basic company and quote info
     quote_sheet.write(0,0, "Symbol:")
@@ -62,15 +59,14 @@ def create_stock_worksheet(stock_symbol):
     quote_sheet.write(6, 0, "# Shares:")
     quote_sheet.write(6, 1, key_stats['SharesOutstanding']) #B7
     quote_sheet.write(7, 0, "EPS:")
-    formula = "B6/B7"
-    quote_sheet.write(7, 1, xlwt.Formula(formula))
+    quote_sheet.write(7, 1, "=B6/B7")
     quote_sheet.write(7, 2, quote['EarningsShare'])
     quote_sheet.write(7, 3, key_stats['ForwardAnnualDividendRate'])
 
 
 
     # create workseet with all dividends
-    div_sheet = stock_book.add_sheet("Dividends")
+    div_sheet = stock_book.add_worksheet("Dividends")
     div_sheet.write(0,0, "Symbol")
     div_sheet.write(0,1, "Date")
     div_sheet.write(0,2, "Dividend")
@@ -80,7 +76,7 @@ def create_stock_worksheet(stock_symbol):
     i = 0
     last_year = div_list[0]['Date'].year
     top = 0
-    div_sheet.col(1).width = (12) * 256
+    div_sheet.set_column('B:B', 12)
     total = 0
     totals = []
 
@@ -95,8 +91,7 @@ def create_stock_worksheet(stock_symbol):
             totals.append({'dollars': total, 'year': last_year})
             total = 0
             last_year = year
-            formula = "SUM(C"+str(top+1)+":C"+str(i)+")"
-            div_sheet.write(i-1, 3, xlwt.Formula(formula), dollar_format)
+            div_sheet.write(i-1, 3, "=SUM(C"+str(top+1)+":C"+str(i)+")", dollar_format)
             top = i
         div_sheet.write(i, 0, div['Symbol'])
         div_sheet.write(i, 1, div['Date'], date_format)
@@ -106,12 +101,11 @@ def create_stock_worksheet(stock_symbol):
 
     # Final yearly total
     totals.append({'dollars': total, 'year': year})
-    formula = "SUM(C"+str(top+1)+":C"+str(i+1)+")"
-    div_sheet.write(i,3, xlwt.Formula(formula), dollar_format)
+    div_sheet.write(i,3, "=SUM(C"+str(top+1)+":C"+str(i+1)+")", dollar_format)
 
 
     # Create yearly totals worksheet
-    year_tot_sheet = stock_book.add_sheet("Yearly Totals")
+    year_tot_sheet = stock_book.add_worksheet("Yearly Totals")
     year_tot_sheet.write(0, 0, "Year")
     year_tot_sheet.write(0, 1, "Total")
     year_tot_sheet.write(0, 2, "Yield")
@@ -127,12 +121,22 @@ def create_stock_worksheet(stock_symbol):
     for div in reversed(totals):
         year_tot_sheet.write(i, 0, div['year'])
         year_tot_sheet.write(i, 1, div['dollars'], dollar_format)
-        formula = "B"+str(i+1)+"/$G$1)"
-        year_tot_sheet.write(i, 2, xlwt.Formula(formula), percent_format)
-        formula = "B"+str(i+1)+"/$I$1"
-        year_tot_sheet.write(i, 3, xlwt.Formula(formula), percent_format)
+        year_tot_sheet.write(i, 2,"=B"+str(i+1)+"/$G$1", percent_format)
+        year_tot_sheet.write(i, 3, "=B"+str(i+1)+"/$I$1", percent_format)
         i += 1
 
+
+    # Create Dividend Yearly Chart
+    chart = stock_book.add_chart({'type': 'line'})
+    # Add a series to the chart
+    chart.add_series({'values': '=\'Yearly Totals\'!$B$2:$B$'+str(i-1),
+                        'categories': '=\'Yearly Totals\'!$A$2:$A$'+str(i-1),
+                        'name': 'Div/Year',
+                        'marker': {'type': 'diamond'},})
+    year_tot_sheet.insert_chart('F3', chart)
+
+
     # Create Excel workbook
-    stock_book.save(os.path.dirname(__file__)+"\\spreadsheets\\"+stock_symbol+".xls")
+    stock_book.close()
+
 
